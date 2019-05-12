76a7e98b236c245d09116b094f62b1429d0037ae
Limitlessdevelopment <limitlessunitydevelopment@gmail.com>
2019-05-13 01:18:17 +0500

    rounded corners

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-de0eda535b449f5ca9d5.js"
+    "url": "webpack-runtime-a307aa2cb5fd3ff472f2.js"
   },
   {
     "url": "app-33a13538e69bfe1191e2.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1926a7075ab3c2175b3a98503c805251"
+    "revision": "b5ff0be5c00bf0640ceda5685017fd06"
   },
   {
     "url": "styles.94cbc7a0262dfe5408cb.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
     "url": "component---src-pages-404-jsx-718fe675ed53b103d685.js"
   },
   {
-    "url": "2-ee110425194fcfbe27b8.js"
+    "url": "2-cd0927e06de596d1c5e2.js"
   },
   {
     "url": "1-c2b0e11a40ab77e4ad75.js"
